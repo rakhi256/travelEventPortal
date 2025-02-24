7f988fadc300ede77aa9844b5bbbdf38d49eba9a
rakhi <rrw@kmd.dk>
2025-02-24 17:32:20 +0530

    Modified DTO and service layer

--- travel-event-portal/src/main/java/com/example/travel_event_portal/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.travel_event_portal.repository;
-
-import com.example.travel_event_portal.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
